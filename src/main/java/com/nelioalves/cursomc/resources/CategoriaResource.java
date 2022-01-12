f36052d8b5e5f670b308c6c39a094c0118a7fe95
Emmanueloldev <pessoalprofissional@hotmail.com>
2022-01-12 15:45:03 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -26,8 +28,12 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service; //ACESSANDO O SERVICE ( CAMADA SERVIÇO )
 
+	
+	
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {  // Método "FIND" pra efetuar operação de "GET", passando "ID" e 
+		
 		//.... retornar CATEGORIA //
 		// <?> indica que pode ser qq tipo//
 		
@@ -36,22 +42,35 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	
 
 	@RequestMapping(method=RequestMethod.POST)	// Anotações do framework para reconhecer o metodo feito abaixo como metodo que vai ser mapeado no EndPoint CATEGORIA e no metodo POST
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){ //@RequestBody faz o JASON ser convertido para o objeto JAVA automaticamente 
-		obj = service.insert(obj); // "Obj" vai ser inserido no Banco de dados, o Bd vai atribuir o novo "Id" para o Objeto obj
-		// pegar o novo "Id" e fornecer como argumento da URI
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){ //@RequestBody faz o JASON ser convertido para o objeto JAVA automaticamente 
+		
+		Categoria obj = service.fromDTO(objDto);
+		obj = service.insert(obj); // "Obj" vai ser inserido no Banco de dados, o Bd vai atribuir o novo "Id" para o Objeto obj//
+		// pegar o novo "Id" e fornecer como argumento da URI//
+		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build(); 
 	}
 	
+	
+	
+	
 	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)      //Mistura do "GET" e do "POST", ele recebe o objeto, e tbm recebe o parametro na URL
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){  // retorna um corpo vazio quando a atualizaçao ocorrer com sucesso, chamamos de update
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){  // retorna um corpo vazio quando a atualizaçao ocorrer com sucesso, chamamos de update
+		Categoria obj = service.fromDTO(objDto);
+		
+		obj.setId(id); 
 		obj =  service.update(obj);                                      // chamar no "service", o obj recebendo "service.update", cria esse metodo la passando o "obj" aqui
-		obj.setId(id);                                                   //se quiser antes de chamar update p garantir que realmente a categoria que vai ser atualizada é que vai ser passada o codigo na URL  
+		                                                  //se quiser antes de chamar update p garantir que realmente a categoria que vai ser atualizada é que vai ser passada o codigo na URL  
 	
 		return ResponseEntity.noContent().build();   // " noContent() = conteudo vazio
 	}
+	
+	
 	
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
@@ -60,13 +79,21 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	
+	
+	
+	
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {  // Método que retonar a lista de categoria DTO
 
 		List <Categoria> list = service.findAll();
 		List <CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+		
 	}
+	
+	
 	
 	@RequestMapping(value="/page", method = RequestMethod.GET)
 	public ResponseEntity<Page<CategoriaDTO>> findPage(
@@ -80,5 +107,8 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(listDto);
 	
 }
+	
+	
+	
 	
 }
