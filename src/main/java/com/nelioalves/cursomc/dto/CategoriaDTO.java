1ad64eb51efcd07b66ce921ff0363fd4da313e87
Emmanueloldev <pessoalprofissional@hotmail.com>
2022-01-25 15:42:41 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente..<<< NAO CONSEGUI CRIAR CNPJ >>>

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -13,7 +13,7 @@ public class CategoriaDTO implements Serializable {
 
 	private Integer id;
 	
-	@NotEmpty(message = "Preenchimento Obrigatório")
+	@NotEmpty(message = "Preenchimento Obrigatório") // Não permite valor nulo e além disso seu tamanho deve ser maior que zero.
 	@Length(min=5, max=80, message= "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
